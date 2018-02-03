d9f138202789454f801765f37a1df0a45d6959fc
Alex Kearns <akearns1010@gmail.com>
2018-02-03 00:11:43 +0000

    Made routing dynamic

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@
     <body>
         @auth
             <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-                <a class="navbar-brand" href="/home">{{ config('app.name') }}</a>
+                <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
