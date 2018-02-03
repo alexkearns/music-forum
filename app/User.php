dc6bb6cbc186ad6f11099d7197c7c7c3fe810440
Matt Williams <mw999@hotmail.co.uk>
2018-02-03 01:16:10 +0000

    User helper methods for posts and threads

--- app/User.php
+++ app/User.php
@@ -5,6 +5,8 @@ namespace App;
 use App\Notifications\ResetPassword;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Post;
+use App\Thread;
 
 class User extends Authenticatable
 {
@@ -27,6 +29,40 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Rules for validating user creates/stores.
+     *
+     * @return array
+     */
+    public static function rulesForCreating()
+    {
+        return [
+            'email'     => ['required', 'email', 'unique:users,email'],
+            'password'  => ['sometimes', 'required', 'between:5,32'],
+            'name'      => ['required', 'between:2,32'],
+        ];
+    }
+
+    /**
+     * Get the posts created by the user.
+     *
+     * @return Post[]
+     */
+    public function getPosts()
+    {
+        return Post::where('user_id', $this->id)->get();
+    }
+
+    /**
+     * Get the threads created by the user.
+     *
+     * @return Thread[]
+     */
+    public function getThreads()
+    {
+        return Thread::where('user_id', $this->id)->get();
+    }
 
     /**
      * Send the password reset notification.
